9bf380a9c83ae27581f929df6ab1a4dcd9bdeb64
mbalves1 <mbalves1@outlook.com>
2024-09-09 08:41:20 -0300

    [feat]: events

--- etherravn.sol
+++ etherravn.sol
@@ -8,9 +8,15 @@ contract etherRavn {
     string fName;
     uint age;
 
+    event Instructor(
+        string name,
+        uint age
+    );
+
     function setInstructor(string memory _fName, uint _age) public {
         fName = _fName;
         age = _age;
+        emit Instructor(_fName, _age);
     }
 
     function getInstructor() public view returns (string memory, uint) {
